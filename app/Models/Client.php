4bb0ab144e20f19b0f3ab39ef0fa7e447c58fe59
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-09-13 23:23:47 -0400

    Defines Client properties

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -9,10 +9,53 @@ use Illuminate\Database\Eloquent\Model;
  * @package App\Models
  *
  * @property int    $id
+ * @property string $name
+ * @property string $identification_number
+ * @property string $credit_card_number
+ * @property float $credit_limit
+ * @property int $person_type_id
+ * @property string $state
  * @property string $created_at
  * @property string $updated_at
  */
 class Client extends Model
 {
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $name;
+    /**
+     * @var string
+     */
+    protected $identification_number;
+    /**
+     * @var string
+     */
+    protected $credit_card_number;
+    /**
+     * @var float
+     */
+    protected $credit_limit;
+    /**
+     * @var id
+     */
+    protected $person_type_id;
+    /**
+     * @var string
+     */
+    protected $state;
+    /**
+     * @var string
+     */
+    protected $created_at;
+    /**
+     * @var string
+     */
+    protected $updated_at;
 
+    protected $fillable = [];
 }
