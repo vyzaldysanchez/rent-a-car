5088e9717667c42fd55b87eb0997e4da1cc69377
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-09-13 22:48:02 -0400

    Defines PersonType model properties

--- app/Models/PersonType.php
+++ app/Models/PersonType.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class PersonType
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $created_at
+ * @property string $updated_at
+ */
 class PersonType extends Model
 {
     const LEGAL_TYPE = 'Legal';
     const PHYSICAL_TYPE = 'Physical';
+
+    /**
+     * @var string
+     */
+    protected $name;
+    /**
+     * @var string
+     */
+    protected $created_at;
+    /**
+     * @var string
+     */
+    protected $updated_at;
+
+    protected $fillable = ['name'];
 }
