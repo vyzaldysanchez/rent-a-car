38c55a5828fc5a43f17ed00b54254419dca82936
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-09-14 23:07:10 -0400

    Defines employee's fillable properties

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -7,9 +7,33 @@ namespace App\Models;
  * @package App\Models
  *
  * @property int    $id
+ * @property string $name
+ * @property string $identification_card
+ * @property string $work_schedule
+ * @property float  $commission_percent
+ * @property string $admission_date
+ * @property string $state
  * @property string $created_at
  * @property string $updated_at
  */
 class Employee extends User
 {
+    protected $id;
+    protected $name;
+    protected $identification_card;
+    protected $work_schedule;
+    protected $commission_percent;
+    protected $admission_date;
+    protected $state;
+    protected $created_at;
+    protected $updated_at;
+
+    protected $fillable = [
+        'name',
+        'identification_card',
+        'work_schedule',
+        'commission_percent',
+        'admission_date',
+        'state'
+    ];
 }
