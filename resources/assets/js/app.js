eaae820a45e9ea778e862c9ddd9fed2772fb49b8
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-09-21 19:19:19 -0400

    Implements dashboard template

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import vClickOutside from 'v-click-outside'
+// Plugins
+import GlobalComponents from './globalComponents'
+import Notifications from './components/UIComponents/NotificationPlugin'
+import SideBar from './components/UIComponents/SidebarPlugin'
+import App from './App'
+// router setup
+import routes from './routes/routes'
+// library imports
+import Chartist from 'chartist'
+import 'bootstrap/dist/css/bootstrap.css'
+import './../sass/paper-dashboard.scss'
+import 'es6-promise/auto'
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+// plugin setup
+Vue.use(VueRouter);
+Vue.use(GlobalComponents);
+Vue.use(vClickOutside);
+Vue.use(Notifications);
+Vue.use(SideBar);
 
-require('./bootstrap');
+// configure router
+const router = new VueRouter({
+    routes, // short for routes: routes
+    linkActiveClass: 'active'
+});
 
-window.Vue = require('vue');
+// global library setup
+Object.defineProperty(Vue.prototype, '$Chartist', {
+    get() {
+        return this.$root.Chartist
+    }
+});
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router,
+    data: {
+        Chartist: Chartist
+    }
 });
