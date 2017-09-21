e75a1eac0e1e0aba2575e21c5be1bab1dd8dd068
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-09-21 19:45:19 -0400

    Defines Rent's fillable fields

--- app/Models/Rent.php
+++ app/Models/Rent.php
@@ -4,7 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Rent
+ * @package App\Models
+ *
+ * @property int    $id
+ * @property string $created_at
+ * @property string $updated_at
+ */
 class Rent extends Model
 {
-    //
+    protected $id;
+    protected $return_date;
+    protected $expected_return_date;
+    protected $daily_fee;
+    protected $duration_in_days;
+    protected $comment;
+    protected $employee_id;
+    protected $client_id;
+    protected $vehicle_id;
+
+    protected $fillable = [
+        'rent_date',
+        'expected_return_date',
+        'return_date',
+        'daily_fee',
+        'duration_in_days',
+        'comment',
+        'state',
+        'employee_id',
+        'vehicle_id',
+        'client_id',
+    ];
 }
