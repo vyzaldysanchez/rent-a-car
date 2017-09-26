c291c103331cdc1b62c4dbf5f8066a7ad01579f6
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-09-26 00:57:53 -0400

    Sets up global utils

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,14 +6,16 @@ import GlobalComponents from './globalComponents'
 import Notifications from './components/UIComponents/NotificationPlugin'
 import SideBar from './components/UIComponents/SidebarPlugin'
 import App from './App.vue'
+import {AUTH_USER_KEY} from './services/user.service'
 // router setup
 import routes from './routes/routes'
 // library imports
-import localforage from 'localforage'
+import * as localforage from 'localforage'
 import Chartist from 'chartist'
 import 'bootstrap/dist/css/bootstrap.css'
 import './../sass/paper-dashboard.scss'
 import 'es6-promise/auto'
+import * as axios from 'axios'
 
 // plugin setup
 Vue.use(VueRouter);
@@ -34,7 +36,7 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-    localforage.getItem('current-user').then(user => {
+    localforage.getItem(AUTH_USER_KEY).then(user => {
         if (user || to.fullPath === '/user/login') {
             next();
         } else {
@@ -46,7 +48,19 @@ router.beforeEach((to, from, next) => {
 // global library setup
 Object.defineProperty(Vue.prototype, '$Chartist', {
     get() {
-        return this.$root.Chartist
+        return this.$root.Chartist;
+    }
+});
+
+Object.defineProperty(Vue.prototype, '$storage', {
+    get() {
+        return localforage;
+    }
+});
+
+Object.defineProperty(Vue.prototype, '$axios', {
+    get() {
+        return axios;
     }
 });
 
