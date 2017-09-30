ecc3995852ed11aa3e85b9685cae75f7f866d70b
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-09-29 23:14:38 -0400

    Displays list of vehicle types

--- resources/assets/js/components/Dashboard/Vehicles/VehicleTypes.vue
+++ resources/assets/js/components/Dashboard/Vehicles/VehicleTypes.vue
@@ -1,23 +1,37 @@
 <template>
     <div class="vehicle-types">
-        <table-list></table-list>
+        <div v-if="!loaded">
+            <h3 class="text-center">Loading data...</h3>
+        </div>
+        <table-list v-if="loaded" :columns="propsToDisplay" :tableData="vehicleTypes"></table-list>
     </div>
 </template>
 <script>
     import TableList from './../Views/TableList.vue'
+
+    const vehicleTypesColumns = ['Ord', 'Description', 'State'];
 
     export default {
         components: {
             TableList
         },
         data() {
-            return {};
+            return {
+                loaded: false,
+                propsToDisplay: [...vehicleTypesColumns],
+                vehicleTypes: []
+            };
         },
         mounted() {
-            this.$axios.get('http://localhost:8000/api/vehicles/types')
-                .then((res) => {
-                    console.log(res);
-                });
+            this.$axios.get('http://localhost:8000/api/types').then((res) => {
+                    this.vehicleTypes = res.data.map((type, position) => {
+                        const {id, description, state} = type;
+
+                        return {ord: position + 1, id, description, state};
+                    });
+                    this.loaded = true;
+                }
+            );
         }
     };
 </script>
--- resources/assets/js/components/Dashboard/Views/TableList.vue
+++ resources/assets/js/components/Dashboard/Views/TableList.vue
@@ -1,31 +1,53 @@
 <template>
     <div class="row">
         <div class="col-md-12">
+            <fg-input name="search-box" placeholder="Search..."
+                      v-model="valueToSearch" @input="filterItems()"></fg-input>
             <div class="card card-plain">
-                <paper-table type="hover" :title="title" :sub-title="subTitle" :data="tableData"
+                <paper-table type="hover" :title="title" :sub-title="subTitle" :data="items"
                              :columns="columns">
-
                 </paper-table>
             </div>
+            <pager :items-per-page="15" :items="this.$props.tableData" @pager-update="updateItemsDisplayed"></pager>
         </div>
     </div>
 </template>
 <script>
     import PaperTable from '../../UIComponents/PaperTable.vue'
+    import Pager from '../../UIComponents/Pager.vue'
 
     export default {
         components: {
+            Pager,
             PaperTable
         },
         props: {
             title: {type: String, default: ''},
             subTitle: {type: String, default: ''},
-            columns: [],
-            tableData: []
+            columns: Array,
+            tableData: Array
+        },
+        computed: {
+            items() {
+                return this.filteredItems.slice(0, 15);
+            }
         },
         data() {
             return {
-
+                valueToSearch: '',
+                filteredItems: this.$props.tableData.slice(0)
+            };
+        },
+        methods: {
+            filterItems() {
+                this.filteredItems = this.$props.tableData.filter(item => {
+                    return Object.keys(item).some(prop => {
+                        return item[prop].toString().toLowerCase().indexOf(this.valueToSearch) !== -1;
+                    });
+                });
+            },
+            updateItemsDisplayed(items) {
+                this.filteredItems = items;
             }
         }
     }
