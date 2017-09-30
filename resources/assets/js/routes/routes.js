1056071bb35b0a105f7f0e2109d7d81daeb06367
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-09-29 23:13:42 -0400

    Fixes Vehicle's routes

--- resources/assets/js/routes/routes.js
+++ resources/assets/js/routes/routes.js
@@ -24,7 +24,7 @@ const routes = [
         component: DashboardLayout,
         redirect: '/admin/stats',
         children: [
-            vehiclesRoutes,
+            ...vehiclesRoutes,
             {
                 path: 'overview',
                 name: 'overview',
--- resources/assets/js/routes/vehicles/routes.js
+++ resources/assets/js/routes/vehicles/routes.js
@@ -1,13 +1,9 @@
 import VehicleTypes from '../../components/Dashboard/Vehicles/VehicleTypes.vue'
 
-export default {
-    path: 'vehicles',
-    component: null,
-    children: [
-        {
-            path: 'types',
-            name: 'vehicle-types',
-            component: VehicleTypes
-        }
-    ]
-};
+export default [
+    {
+        path: 'vehicles/types',
+        name: 'vehicle-types',
+        component: VehicleTypes
+    }
+];
