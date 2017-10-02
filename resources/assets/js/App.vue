1b6b3161b44768acc9365a6f0d370c03e7bc2322
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-10-02 18:42:20 -0400

    Removes unnecessary menu items

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -5,21 +5,6 @@
         <side-bar type="navbar" :sidebar-links="$sidebar.sidebarLinks">
             <ul class="nav navbar-nav">
                 <li>
-                    <a class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="ti-panel"></i>
-                        <p>Stats</p>
-                    </a>
-                </li>
-                <drop-down title="5 Notifications" icon="ti-bell">
-
-                    <li><a>Notification 1</a></li>
-                    <li><a>Notification 2</a></li>
-                    <li><a>Notification 3</a></li>
-                    <li><a>Notification 4</a></li>
-                    <li><a>Another notification</a></li>
-
-                </drop-down>
-                <li>
                     <a>
                         <i class="ti-settings"></i>
                         <p>Settings</p>
