49c2f874eca17b0c8930a77a0802afda09da2ac8
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-10-11 18:23:32 -0400

    Adds Vehicles main component

---
+++ resources/assets/js/components/Dashboard/Vehicles/Forms/VehiclesForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="vehicles-form">
+
+        <notifications></notifications>
+    </div>
+</template>
+<script>
+    import PaperNotification from '../../../UIComponents/NotificationPlugin/Notifications.vue';
+
+    export default {
+        components: {
+            PaperNotification
+        },
+        data() {
+            return {};
+        }
+    };
+</script>
---
+++ resources/assets/js/components/Dashboard/Vehicles/Vehicles.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="vehicles">
+        <div v-if="!isLoaded">
+            <h3>Loading...</h3>
+        </div>
+        <div v-if="isLoaded">
+            Loaded
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                isLoaded: false,
+                vehicles: []
+            };
+        },
+        mounted() {
+            this.$axios.get('http://localhost:8000/api/vehicles')
+                .then(response => {
+                    this.vehicles = response.data;
+                    this.isLoaded = true;
+                });
+        }
+    };
+</script>
