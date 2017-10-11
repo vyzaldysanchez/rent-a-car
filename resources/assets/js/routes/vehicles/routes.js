e4bcde1b92ce8d5280abc7989f30b64dcc9ebbf7
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-10-11 01:01:06 -0400

    Adds Fuels Menu

--- resources/assets/js/routes/vehicles/routes.js
+++ resources/assets/js/routes/vehicles/routes.js
@@ -1,9 +1,9 @@
-import VehicleTypes from '../../components/Dashboard/Vehicles/VehicleTypes.vue'
-import VehicleBrands from '../../components/Dashboard/Vehicles/VehicleBrands.vue'
-import VehicleModels from '../../components/Dashboard/Vehicles/VehicleModels.vue'
+import VehicleTypes from '../../components/Dashboard/Vehicles/VehicleTypes.vue';
+import VehicleBrands from '../../components/Dashboard/Vehicles/VehicleBrands.vue';
+import VehicleModels from '../../components/Dashboard/Vehicles/VehicleModels.vue';
+import VehicleFuels from '../../components/Dashboard/Vehicles/VehicleFuels.vue';
 
-export default [
-    {
+export default [{
         path: 'vehicles/types',
         name: 'vehicle-types',
         component: VehicleTypes
@@ -17,5 +17,10 @@ export default [
         path: 'vehicles/models',
         name: 'vehicle-models',
         component: VehicleModels
+    },
+    {
+        path: 'vehicles/fuels',
+        name: 'vehicle-fuels',
+        component: VehicleFuels
     }
 ];
