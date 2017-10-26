cdd3112834e0f8fce5d99762eb6f4793f35c3c55
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-10-26 18:54:50 -0400

    Enables vehicles removal

--- resources/assets/js/components/Dashboard/Vehicles/Vehicles.vue
+++ resources/assets/js/components/Dashboard/Vehicles/Vehicles.vue
@@ -36,6 +36,7 @@
                 .then(response => {
                     this.vehicles = response.data.map((vehicle, index) => {
                         const mappedVehicle = {
+                            id: vehicle.id,
                             description: vehicle.description,
                             chassis: vehicle['chassis_number'],
                             engine: vehicle['engine_number'],
@@ -47,7 +48,12 @@
                             state: vehicle.state,
                         };
 
-                        return factory.createForTableList({object: mappedVehicle, index, onEdit: null, onRemove: null});
+                        return factory.createForTableList({
+                            object: mappedVehicle,
+                            index,
+                            onEdit: null,
+                            onRemove: this.askToRemove
+                        });
                     });
 
                     this.isLoaded = true;
@@ -56,6 +62,7 @@
         methods: {
             addVehicle(data) {
                 const mappedVehicle = {
+                        id: data.id,
                         description: data.description,
                         chassis: data['chassis_number'],
                         engine: data['engine_number'],
@@ -72,8 +79,24 @@
                     object: mappedVehicle,
                     index,
                     onEdit: null,
-                    onRemove: null
+                    onRemove: this.askToRemove
                 }));
+            },
+            askToRemove(vehicle) {
+                this.$swal({
+                    title: 'Are you sure?',
+                    text: `The vehicle and all it\'s data associated will be deleted.`,
+                    type: 'warning',
+                    showConfirmButton: true,
+                    showCancelButton: true
+                }).then(() => this.removeVehicle(vehicle));
+            },
+            removeVehicle(vehicle) {
+                this.$axios.delete(`http://localhost:8000/api/vehicles/${vehicle.id}`)
+                    .then(() => {
+                        const index = this.vehicles.findIndex(storedVehicle => vehicle.id === storedVehicle.id);
+                        this.vehicles = this.vehicles.slice(0, index).concat(this.vehicles.slice(index + 1));
+                    });
             }
         }
     };
