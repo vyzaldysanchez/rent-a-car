c6bf491a9ea89566711e4a77267066532946e98c
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-11-20 19:19:02 -0400

    Creates fgTextarea component

---
+++ resources/assets/js/components/UIComponents/Inputs/formGroupTextarea.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text-area">
+      <label :for="id">{{label}}</label>
+      <textarea :name="name" :id="id" :cols="cols" :rows="rows" v-bind="$props" :minlength="min" :maxlength="max" @input="$emit('input',$event.target.value)"
+               @blur="$emit('blur', $event.target)" @change="$emit('change', $event)"></textarea>
+  </div>
+</template>
+
+<script>
+import fgInput from './formGroupInput.vue';
+
+export default {
+  ...fgInput,
+  props: {
+    ...fgInput.props,
+    id: String,
+    cols: Number,
+    rows: Number
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  display: block;
+  width: 100%;
+  height: 10em;
+}
+</style>
