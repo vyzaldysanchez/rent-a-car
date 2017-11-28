43cff687674138304d35c522bca1e8a94b059d2d
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-11-28 19:05:54 -0400

    Enhances uutils/helpers

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -19,6 +19,8 @@ import './../sass/paper-dashboard.scss';
 import 'es6-promise/auto';
 import * as axios from 'axios';
 import dataFormatter from './utils/data-formatter.utils';
+import formsValidator from './utils/form-validator.utils';
+import dateHelper from './utils/date.utils';
 
 // plugin setup
 Vue.use(VueRouter);
@@ -31,56 +33,68 @@ Vue.use(LoadingMessage);
 
 // Storage setup
 localforage.default.config({
-  name: 'vy-rent-car'
+	name: 'vy-rent-car'
 });
 
 // configure router
 const router = new VueRouter({
-  routes, // short for routes: routes
-  linkActiveClass: 'active'
+	routes, // short for routes: routes
+	linkActiveClass: 'active'
 });
 
 router.beforeEach((to, from, next) => {
-  localforage.getItem(AUTH_USER_KEY).then(user => {
-    if (user || to.fullPath === '/user/login') {
-      next();
-    } else {
-      next({ path: '/user/login' });
-    }
-  });
+	localforage.getItem(AUTH_USER_KEY).then(user => {
+		if (user || to.fullPath === '/user/login') {
+			next();
+		} else {
+			next({ path: '/user/login' });
+		}
+	});
 });
 
 // global library setup
 Object.defineProperty(Vue.prototype, '$Chartist', {
-  get() {
-    return this.$root.Chartist;
-  }
+	get() {
+		return this.$root.Chartist;
+	}
 });
 
 Object.defineProperty(Vue.prototype, '$storage', {
-  get() {
-    return localforage;
-  }
+	get() {
+		return localforage;
+	}
 });
 
 Object.defineProperty(Vue.prototype, '$axios', {
-  get() {
-    return axios;
-  }
+	get() {
+		return axios;
+	}
 });
 
 Object.defineProperty(Vue.prototype, '$formatter', {
-  get() {
-    return dataFormatter;
-  }
+	get() {
+		return dataFormatter;
+	}
+});
+
+Object.defineProperty(Vue.prototype, '$formsValidator', {
+	get() {
+		return formsValidator;
+	}
+});
+
+Object.defineProperty(Vue.prototype, '$date', {
+	get() {
+		return dateHelper;
+	}
 });
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App),
-  router,
-  data: {
-    Chartist
-  }
+	el: '#app',
+	render: h => h(App),
+	router,
+	data: {
+		Chartist
+	}
 });
--- resources/assets/js/utils/form-validator.utils.js
+++ resources/assets/js/utils/form-validator.utils.js
@@ -2,28 +2,37 @@ const MULTIPLIERS = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1];
 const IDENTIFICATION_LENGTH = 11;
 
 export default {
-    validateIdentification(identification = '') {
-        const id = identification.replace(/-+/g, ''),
-            idLength = id.length;
+	validateIdentification(identification = '') {
+		const id = identification.replace(/-+/g, ''),
+			idLength = id.length;
 
-        if (idLength !== IDENTIFICATION_LENGTH) {
-            return false;
-        }
+		if (idLength !== IDENTIFICATION_LENGTH) {
+			return false;
+		}
 
-        let total = 0;
-        let digit = 0;
+		let total = 0;
+		let digit = 0;
 
-        for (; digit < IDENTIFICATION_LENGTH; digit++) {
-            const num = Number.parseFloat(id[digit]) * MULTIPLIERS[digit];
+		for (; digit < IDENTIFICATION_LENGTH; digit++) {
+			const num = Number.parseFloat(id[digit]) * MULTIPLIERS[digit];
 
-            if (num < 10) {
-                total += num;
-            } else {
-                const numString = num.toString();
-                total += Number.parseFloat(numString[0]) + Number.parseFloat(numString[1]);
-            }
-        }
+			if (num < 10) {
+				total += num;
+			} else {
+				const numString = num.toString();
+				total +=
+					Number.parseFloat(numString[0]) +
+					Number.parseFloat(numString[1]);
+			}
+		}
 
-        return total % 10 === 0;
-    }
+		return total % 10 === 0;
+	},
+	getErrorListAsHTML(errors = []) {
+		const errorList = errors.reduce(
+			(htmlErrors, error) => `${htmlErrors}<li>${error}</li>`,
+			``
+		);
+		return `<ul>${errorList}</ul>`;
+	}
 };
