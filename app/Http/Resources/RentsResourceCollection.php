a04f328ca5d6a66246ca76bea252a53c2391b73e
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-11-30 01:07:23 -0400

    Fetchs rents to display in table

---
+++ app/Http/Resources/RentsResourceCollection.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class RentsResourceCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
